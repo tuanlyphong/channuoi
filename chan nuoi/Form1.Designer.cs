99afa1c6b3dfa2e84dd15908fb7c3e0ce70bcf7e
tuanphong <tuanphong@NTRLORD>
2023-12-04 11:16:22 +0700

    Add project files.

---
+++ chan nuoi/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace chan_nuoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(330, 226);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 36);
+            button1.TabIndex = 0;
+            button1.Text = "login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Location = new Point(135, 140);
+            label1.Name = "label1";
+            label1.Size = new Size(97, 25);
+            label1.TabIndex = 1;
+            label1.Text = "username:";
+          
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Location = new Point(138, 184);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 25);
+            label2.TabIndex = 2;
+            label2.Text = "password:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(238, 133);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(172, 32);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(238, 177);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(172, 32);
+            textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(41, 173, 178);
+            ClientSize = new Size(493, 370);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Font = new Font("Impact", 12F);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+       
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ chan nuoi/Form1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+namespace chan_nuoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data source=NTRLORD; Initial Catalog=channuoi; Integrated Security=true ");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open(); 
+            SqlCommand cmd = new SqlCommand("sp_login", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@username", SqlDbType.NVarChar).Value = textBox1.Text;
+            cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = textBox2.Text;
+            SqlDataReader dr = cmd.ExecuteReader();
+                if(dr.Read())
+            {
+                Dashboard d = new Dashboard();
+                d.Show();
+                this.Hide();
+            }
+                else
+            {
+                MessageBox.Show("dang nhap that bai");
+            }
+                con.Close();
+        }
+    }
+}
